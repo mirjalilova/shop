a616e8e5ad0bfeb23c888d9e387b8c001cac3878
mirjalilova <feruzamirjalilova767@gmail.com>
2025-07-28 18:46:53 +0500

    fixed

--- migrations/000001_migrate_name.up.sql
+++ migrations/000001_migrate_name.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     debt BIGINT NOT NULL DEFAULT 0,
-    role role NOT NULL DEFAULT 'news',
+    role role NOT NULL DEFAULT 'user',
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     deleted_at BIGINT NOT NULL DEFAULT 0
@@ -23,10 +23,10 @@ CREATE TABLE IF NOT EXISTS category (
 CREATE TABLE IF NOT EXISTS shoes (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
-    size []INT NOT NULL,
-    color []VARCHAR(20) NOT NULL,
+    size INT[] NOT NULL, 
+    color VARCHAR(20)[] NOT NULL, 
     price FLOAT NOT NULL,
-    img_url []TEXT NOT NULL,
+    img_url TEXT[] NOT NULL,
     category_id UUID NOT NULL REFERENCES category(id) ON DELETE CASCADE,
     description TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
